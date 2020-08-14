c5549d0424bbbea7863b34841b89ce2666f47b03
某亚瑟 <i@yojigen.tech>
2020-08-14 16:45:28 +0800

    Update

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  publicPath: ''
+    publicPath: '',
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = '大兴安岭职业学院邮箱系统'
+                return args
+            })
+    },
 }
